1cb39fadc4838aa7e2acc73b28851dc416a16ffe
George <64532466+Nu1Lifier@users.noreply.github.com>
2020-09-14 15:37:01 +0300

    Initial commit

---
+++ app/src/main/java/com/Nu1Lifier/trafficlight/MainActivity.java
@@ -0,0 +1,75 @@
+package com.Nu1Lifier.trafficlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout b_1, b_2, b_3;
+    private Button button_1;
+    private boolean start_stop = false;
+    private int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b_1 = findViewById(R.id.bulb_1);
+        b_2 = findViewById(R.id.bulb_2);
+        b_3 = findViewById(R.id.bulb_3);
+        button_1 = findViewById(R.id.button_start_stop);
+    }
+
+    public void onClickStart(View view) {
+        if (!start_stop ){
+            start_stop = true;
+            button_1.setText("Stop");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (start_stop){
+                        counter++;
+                        switch (counter){
+                            case 1:
+                                b_1.setBackgroundColor(Color.RED);
+                                b_2.setBackgroundColor(Color.GRAY);
+                                b_3.setBackgroundColor(Color.GRAY);
+                                break;
+                            case 2:
+                                b_1.setBackgroundColor(Color.GRAY);
+                                b_2.setBackgroundColor(Color.YELLOW);
+                                b_3.setBackgroundColor(Color.GRAY);
+                                break;
+                            case 3:
+                                b_1.setBackgroundColor(Color.GRAY);
+                                b_2.setBackgroundColor(Color.GRAY);
+                                b_3.setBackgroundColor(Color.GREEN);
+                                counter = 0;
+                                break;
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        } else {
+            start_stop = false;
+            button_1.setText("Start");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        start_stop = false;
+    }
+}
